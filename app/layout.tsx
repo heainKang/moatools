b89e3c6b306c81294075550d8d082e11ce7f016c
강해인 <kangheain@ganghaein-ui-MacBookAir.local>
2026-05-14 22:34:07 +0900

    Search Console 인증 태그 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,6 +26,7 @@ export default function RootLayout({
   return (
     <html lang="ko" className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}>
       <head>
+        <meta name="google-site-verification" content="cZeNnsj93st33DvE3YIoJQ8kE1uRiAWk8leu_IfdUKI" />
         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3382986214425775" crossOrigin="anonymous"></script>
       </head>
       <body className="min-h-full flex flex-col">{children}</body>
